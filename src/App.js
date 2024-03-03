45052ab083288d1bad1c4a99963fe85e53dbb3ef
annssva <sysoevaannaa@mail.ru>
2024-03-03 16:29:46 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [dots, setDots] = useState([]);
+
+    const handleButtonClick = () => {
+        setCount(count + 1);
+        createDot();
+    };
+
+    const createDot = () => {
+        const newDot = {
+            id: count,
+            left: Math.random() * window.innerWidth - 10,
+            top: Math.random() * window.innerHeight - 10,
+        };
+        setDots((prevDots) => [...prevDots, newDot]);
+    };
+
+    return (
+        <>
+            <div className="main-div">
+                <h1 className="title">Кликер</h1>
+                <div className="count-div">
+                    <p className="count">{count !== 0 && count}</p>
+                </div>
+                <button onClick={handleButtonClick} className="btn">
+                    Кликни
+                </button>
+            </div>
+            {dots.map((dot) => (
+                <div key={dot.id} className="dot" style={{ left: `${dot.left}px`, top: `${dot.top}px` }} />
+            ))}
+        </>
+
+    );
 }
 
 export default App;
